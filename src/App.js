caccf80045281ff33b9feeca5dc87b2258fbdefe
AlexFedorko <alexfedorko15@gmail.com>
2021-07-08 17:39:37 +0300

    init new repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import {useDispatch, useSelector} from 'react-redux'
+import {useEffect} from "react";
+
+
+
+
+const NestedChild = () => {
+    const counter = useSelector((state) => {
+        console.log('from useSelector', state)
+
+        return state.counter.value
+    })
+
+    const posts = useSelector(({posts}) => posts);
+    const dispatch = useDispatch()
+
+    const fetchPosts = async () => {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const json = await response.json();
+
+        console.log(json)
+        dispatch({
+            type: 'ADD_POSTS',
+            payload: json
+        })
+
+    }
+
+
+    useEffect(() => {
+        fetchPosts();
+    }, [])
+
+
+console.log(posts, 'posts')
+
+
+
+// Коротший синтаксис
+// const NestedChild = () => {
+//     const counter = useSelector(({counter: {value}}) => value)
+
+
+
+  return(
+
+
       <header className="App-header">
+          <h1>{counter}</h1>
+
+
+          <ul>
+              {
+                  posts.map(post => {
+                      <li>{post.title}</li>
+                  })
+
+              }
+          </ul>
+
+
+
+          <button onClick={() => {
+            dispatch({type: 'INC-Custom', payload: +prompt('Type number')})
+          }}> Increment</button>
+
+          <button onClick={() => {
+          dispatch(
+              {type: 'DEC'}
+          )
+          }}>Decrement</button>
+
+          <button onClick={() => {
+              dispatch(
+                  {type: 'RESET'}
+              )
+          }}>Reset</button>
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
         >
           Learn React with okten
         </a>
+
       </header>
+  )
+
+}
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+        <NestedChild/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,85 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from 'react-redux'
+
+const initilState = {
+    counter: {
+        value: 0
+    },
+    posts: []
+
+}
+
+
+
+const reducer = (state = initilState, action) => {
+    switch (action.type){
+        // case 'INC': {
+        //    return{
+        //        ...state,
+        //        counter: {
+        //            value: state.counter.value + 1
+        //        }
+        //    }
+        // }
+
+        case 'INC-Custom' : {
+            return {
+                ...state,
+                counter: {
+                    value: action.payload
+                }
+
+            }
+        }
+
+        case 'DEC': {
+            return {
+                ...state,
+                counter: {
+                    value: state.counter.value - 1
+                }
+            }
+        }
+
+
+        case 'RESET': {
+            return {
+                ...state,
+                counter: {
+                    value: 0
+                }
+            }
+        }
+
+        case 'ADD_POSTS': {
+            return {
+                ...state,
+               posts: action.payload
+            }
+        }
+
+
+
+        default:
+            return state
+    }
+}
+
+
+const store = createStore(reducer)
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+      </Provider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
